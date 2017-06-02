ee5f4d772a1cbd1def17aaa88533f1e48b431ef5
corbettbain <corbettbain@gmail.com>
2017-06-02 14:46:12 +0800

    1

--- src/main/java/com/jiaqu/icb/service/icbrec/impl/ICBOrderMessageRecServiceimpl.java
+++ src/main/java/com/jiaqu/icb/service/icbrec/impl/ICBOrderMessageRecServiceimpl.java
@@ -6,11 +6,12 @@ import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.log4j.Logger;
-import org.springframework.stereotype.Service;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.icbc.api.codec.Base64;
 import com.icbc.api.core.ApiClient;
+import com.icbc.api.core.ApiException;
 import com.jiaqu.icb.enums.IcbOrderMessageRecServiceEnum;
 import com.jiaqu.icb.service.icbrec.ICBOrderMessageRecService;
 
@@ -20,12 +21,16 @@ import com.jiaqu.icb.service.icbrec.ICBOrderMessageRecService;
  *
  */
 
-@Service
-public class ICBOrderMessageRecServiceimpl implements ICBOrderMessageRecService {
+public class ICBOrderMessageRecServiceImpl implements ICBOrderMessageRecService {
 
 	
-	private Logger logger = Logger.getLogger(ICBOrderMessageRecServiceimpl.class);
+	private Logger logger = LoggerFactory.getLogger(ICBOrderMessageRecServiceImpl.class);
 	
+	private static final String RESULT_SIGNATURE_FAIL = "验签不通过";
+	
+	private static final String RESULT_SIGNATURE_SUCCESS = "验签通过";
+	
+	private static final String RESULT_TRANSTYPE_FAIL = "TRANSTYPE不符";
 	/**
 	 * 订单通知的业务层,接收通知并且验证签名
 	 * @author 周宁
@@ -33,7 +38,7 @@ public class ICBOrderMessageRecServiceimpl implements ICBOrderMessageRecService 
 	 *
 	 */
 	@Override
-	public Object orderMessageRec(HttpServletRequest request,HttpServletResponse response) throws Exception {
+	public Map<String, Object> orderMessageRec(HttpServletRequest request,HttpServletResponse response) throws Exception {
 		logger.info("ICBOrderMessageRecServiceimpl : 开始获取订单通知参数");
 		System.out.println("支付结果通知...");
 		String transtype = request.getParameter(IcbOrderMessageRecServiceEnum.transtype.toString());
@@ -51,30 +56,56 @@ public class ICBOrderMessageRecServiceimpl implements ICBOrderMessageRecService 
 		
 		logger.info("ICBOrderMessageRecServiceimpl : 参数为:transtype:" + transtype + "----version:" + version +"----reqdata:" + reqdata + "-----apigw_sign:" + apigw_sign + "----apigw_timestamp:" + apigw_timestamp + "----apigw_certid:" + apigw_certid);
 		
-		logger.info("ICBOrderMessageRecServiceimpl : 开始开始封装参数进行验签");
-		Map<String,String> paramMap = new HashMap<String,String>();
+		return signature(transtype, version, apigw_timestamp
+					, apigw_certid, reqdata, apigw_sign, IcbOrderMessageRecServiceEnum.charset);
+	}
+
+	@Override
+	public Map<String, Object> orderMessageRec(String transtype, String version, String apigw_timestamp,
+			String apigw_certid, String reqdata, String apigw_sign) throws Exception {
+		logger.info("ICBOrderMessageRecServiceimpl : 开始获取订单通知参数");
+		
+		return signature(transtype, version, apigw_timestamp
+				, apigw_certid, reqdata, apigw_sign, IcbOrderMessageRecServiceEnum.charset);
+	}
+
+	
+	
+
+	
+	private Map<String, Object> signature(String transtype,String version,String apigw_timestamp,String apigw_certid,String reqdata,String apigw_sign,IcbOrderMessageRecServiceEnum charset) throws ApiException{
+		logger.info("ICBOrderMessageRecServiceImpl : 开始开始封装参数进行验签");
+		Map<String, String> paramMap = new HashMap<String,String>();
 		paramMap.put(IcbOrderMessageRecServiceEnum.transtype.toString(), transtype);
 		paramMap.put(IcbOrderMessageRecServiceEnum.version.toString(), version);
 		paramMap.put(IcbOrderMessageRecServiceEnum.apigw_timestamp.toString(), apigw_timestamp);
 		paramMap.put(IcbOrderMessageRecServiceEnum.apigw_certid.toString(), apigw_certid);
 		paramMap.put(IcbOrderMessageRecServiceEnum.reqdata.toString(), reqdata);
 
-		logger.info("ICBOrderMessageRecServiceimpl : 参数为:" + paramMap);
+		logger.info("ICBOrderMessageRecServiceImpl : 参数为:" + paramMap + "----apigw_sign:" + apigw_sign);
+		
+		Map<String, Object> map = new HashMap<>();
+		map.put(ICBOrderMessageRecService.REC_DATA,paramMap);
 		
 		if(transtype.equals(IcbOrderMessageRecServiceEnum.transtype.getValue())){
 			ApiClient ac = new ApiClient(null,IcbOrderMessageRecServiceEnum.pubceraddr.getValue());
-			try{
-				boolean isOK = ac.doVerify(paramMap,apigw_sign,IcbOrderMessageRecServiceEnum.charset.getValue());
-				if(isOK){
-					System.out.println("验签通过...");
-					logger.info("ICBOrderMessageRecServiceimpl : 验签通过");
-				}
-			}catch(Exception e){
-				System.out.printf(e.getMessage(),e);
+			boolean isOK = ac.doVerify(paramMap,apigw_sign,charset.getValue());
+			if(isOK){
+				System.out.println("验签通过...");
+				logger.info("ICBOrderMessageRecServiceImpl : 验签通过");
+				map.put(IS_OK, true);
+				map.put(RESULT, RESULT_SIGNATURE_SUCCESS);
+				return map;
+			}else {
+				map.put(IS_OK, false);
+				map.put(RESULT, RESULT_SIGNATURE_FAIL);
+				return map;
 			}
-		}
-		
-		return null;
+		}else {
+			map.put(IS_OK, false);
+			map.put(RESULT, RESULT_TRANSTYPE_FAIL);
+			return map;
+		}	
 	}
-
+	
 }
